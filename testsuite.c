ed334fbdab0073b3ea03dbeeebe26ef2a2268b44
auffret.c@stjolorient.fr <auffret.c@stjolorient.fr>
2025-01-02 22:57:27 +0100

    Retour main

--- testsuite.c
+++ testsuite.c
@@ -45,6 +45,7 @@ void max_test_3(void) {
 int main ( void )
 {
    CU_pSuite pSuite = NULL;
+   CU_ErrorCode status;
 
    /* initialize the CUnit test registry */
    if ( CUE_SUCCESS != CU_initialize_registry() )
@@ -68,20 +69,13 @@ int main ( void )
    }
 
    // Run all tests using the basic interface
- /* CU_basic_set_mode(CU_BRM_VERBOSE);
-   CU_basic_run_tests();
+   CU_basic_set_mode(CU_BRM_NORMAL);
+   status = CU_basic_run_tests();
    printf("\n");
    CU_basic_show_failures(CU_get_failure_list());
    printf("\n\n");*/
 
-   // Run all tests using the automated interface
-   CU_automated_run_tests();
-   CU_list_tests_to_file();
-
-   // Run all tests using the console interface
-   CU_console_run_tests();
-
    /* Clean up registry and return */
    CU_cleanup_registry();
-   return CU_get_error();
+   return status;
 }
