62804095806174fea633a8e41d691ec7363d74f3
auffret.c@stjolorient.fr <auffret.c@stjolorient.fr>
2025-01-11 18:05:39 +0100

    Ajout encodage

--- Python/file_utils.py
+++ Python/file_utils.py
@@ -3,8 +3,7 @@
 def diff(file_first, file_second):
     """Fonction vérifiant si deux fichiers sont différents."""
     result = False
-    with open(file_first, "r") as file_first_id :
-        with open(file_second, "r") as file_second_id :
+    with open(file_first, , mode="r", encoding="utf-8") as file_first_id :
+        with open(file_second, , mode="r", encoding="utf-8") as file_second_id :
             result=file_first_id.read() != file_second_id.read()
     return result
-
