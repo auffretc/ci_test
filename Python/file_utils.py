6d45ba2b40f0647970c01538e61f3ff3bbe4edf6
auffret.c@stjolorient.fr <auffret.c@stjolorient.fr>
2025-01-11 18:02:06 +0100

    orrection quelques erreurs pylint

--- Python/file_utils.py
+++ Python/file_utils.py
@@ -1,23 +1,10 @@
-import tempfile
-
+"""Module comparant des fichiers texte."""
 
 def diff(file_first, file_second):
-    file_first_id = open(file_first, "r")
-    file_second_id = open(file_second, "r")
+    """Fonction vérifiant si deux fichiers sont différents."""
+    result = False
+    with open(file_first, "r") file_first_id :
+        with open(file_second, "r") file_second_id :
+            result=file_first_id.read() != file_second_id.read()
+    return result
 
-    return file_first_id.read() != file_second_id.read()
-"""
-if __name__ == "__main__":
-    file_first = tempfile.mktemp()
-    file_second = tempfile.mktemp()
-    file_not_found = tempfile.mktemp()
-
-
-    with open(file_first, "w+") as file_first_id:
-        file_first_id.write("")
-
-    with open(file_second, "w+") as file_second_id:
-        file_second_id.write("Hello World!\n")
-    
-    print("result = %d, result = %d" % (diff(file_first, file_first), diff(file_first, file_second)))
-"""
