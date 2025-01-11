91295807b707412cfe1723fc75872f18170e6e9c
auffret.c@stjolorient.fr <auffret.c@stjolorient.fr>
2025-01-11 18:03:45 +0100

    Correction  bloc with

--- Python/file_utils.py
+++ Python/file_utils.py
@@ -3,8 +3,8 @@
 def diff(file_first, file_second):
     """Fonction vérifiant si deux fichiers sont différents."""
     result = False
-    with open(file_first, "r") file_first_id :
-        with open(file_second, "r") file_second_id :
+    with open(file_first, "r") as file_first_id :
+        with open(file_second, "r") as file_second_id :
             result=file_first_id.read() != file_second_id.read()
     return result
 
