36b238d685fa37eb1e16b359d62822971f65099e
auffret.c@stjolorient.fr <auffret.c@stjolorient.fr>
2025-01-11 18:33:11 +0100

    Nettoyage de code

--- Python/file_utils.py
+++ Python/file_utils.py
@@ -4,6 +4,5 @@ def diff(file_first, file_second):
     """Fonction vérifiant si deux fichiers sont différents."""
     result = False
     with open(file_first, "r", encoding="utf-8") as file_first_id :
-        with open(file_second, "r", encoding="utf-8") as file_second_id :
-            result=file_first_id.read() != file_second_id.read()
+        result=file_first_id.read()!=""
     return result
