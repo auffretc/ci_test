a44a0cbb48134e97054bab7907e08ec97907527c
auffret.c@stjolorient.fr <auffret.c@stjolorient.fr>
2025-01-12 10:23:55 +0100

    Modification commentaires.

--- Python/test_file_utils.py
+++ Python/test_file_utils.py
@@ -4,9 +4,9 @@ import tempfile
 import file_utils
 
 class TestFileUtils(unittest.TestCase):
-    """file_utils unit testing."""
+    """tests unitaires de file_utils."""
     def setUp(self):
-        """Test fixture setup: called before each test."""
+        """Montage de la fixation de test : appelé avant chaque cas de test."""
         _, self.file_first_name = tempfile.mkstemp()
         _, self.file_second_name = tempfile.mkstemp()
         _, self.file_not_found = tempfile.mkstemp()
@@ -20,25 +20,25 @@ class TestFileUtils(unittest.TestCase):
         os.remove(self.file_not_found)
 
     def tearDown(self):
-        """Test fixture teardown: called after each test."""
+        """Démontage de la fixation de test : appelé après cahque cas de test."""
         os.remove(self.file_first_name)
         os.remove(self.file_second_name)
     
     def testAreSame(self):
-        """Test case that verifies the files are identical."""
-        assert not file_utils.diff(self.file_first_name, self.file_first_name), "similarity not detected"
+        """Cas de test vérifiant que les fichiers sont identiques."""
+        assert not file_utils.diff(self.file_first_name, self.file_first_name), "similitude non détectée"
     
     def testAreDifferent(self):
-        """Test case that verifies the file are not identical."""
-        assert file_utils.diff(self.file_first_name, self.file_second_name), "difference not detected"
+        """Cas de test vérifiant que les fichiers sont différents."""
+        assert file_utils.diff(self.file_first_name, self.file_second_name), "différence non détectée"
 
     def testFirstFileNotFound(self):
-        """Test case that verifies the first file is not found."""
+        """Cas de test vérifiant que le premier fichier n'est pas trouvé."""
         with self.assertRaises(FileNotFoundError):
             file_utils.diff(self.file_not_found, self.file_second_name)
 
     def testSecondFileNotFound(self):
-        """Test case that verifies the second file is not found."""
+        """Cas de test vérifiant que le second fichier n'est pas trouvé."""
         with self.assertRaises(FileNotFoundError):
             file_utils.diff(self.file_first_name, self.file_not_found)
 
