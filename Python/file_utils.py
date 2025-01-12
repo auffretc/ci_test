7d739a2369c419f0b4d949509a5fa539a790e9eb
auffret.c@stjolorient.fr <auffret.c@stjolorient.fr>
2025-01-12 11:33:04 +0100

    Ajout erreur

--- Python/file_utils.py
+++ Python/file_utils.py
@@ -5,5 +5,5 @@ def diff(file_first, file_second):
     result = False
     with open(file_first, mode="r", encoding="utf-8") as file_first_id:
         with open(file_second, mode="r", encoding="utf-8") as file_second_id:
-            result = file_first_id.read() != file_second_id.read()
+            result = file_first_id.read() == file_second_id.read()
     return result
